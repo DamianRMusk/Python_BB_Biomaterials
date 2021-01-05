6b453846f9b643cd3d6b2d66c88e2d749bb87199
Damian Musk <damianmusk@gmail.com>
2021-01-04 21:34:06 -1000

    Added more detailed documentation and cleaned up formatting

--- BB_Biomaterials.py
+++ BB_Biomaterials.py
@@ -1,3 +1,12 @@
+#!/usr/bin/env python
+
+"""
+The following program, written by D. R. Musk, takes the Bergström-Boyce model with a Williams-Landel-Ferry shift 
+representation for time-temperature superposition, evaluating stress-strain behavior in polymers (given 
+biomaterial data: cellulose, xylan hemicellulose, and high-methoxyl pectin) under an extra climate stressor. 
+Procedure and inverse Langevin approximation inspired by DOI: 10.1016/s0022-5096(97)00075-6.
+"""
+
 import scipy.integrate 
 from matplotlib.pyplot import *
 from numpy import *
@@ -5,10 +14,10 @@ from numpy.linalg import *
 from pylab import *
 import scipy.optimize
 
-def ramp(x):
+def ramp(x): # Mathematical ramp functions
     return (x + abs(x)) / 2.0
 
-def invLangevin(x): 
+def inv_Langevin(x): # Mathematical inverse Langevin function
     EPS = spacing(1)
     if type(x) == float or type(x) == float64:
         if x >= 1-EPS: x = 1 - EPS
@@ -25,7 +34,7 @@ def invLangevin(x):
     res[index] = 1.0 / (sign(x[index]) - x[index])
     return res 
 
-def EC_3D(stretch, param):
+def EC_3D(stretch, param): # Function defining used material tensors in eight chain model, completing procedure item 1
     L1 = stretch[0]
     L2 = stretch[1] 
     L3 = stretch[2] 
@@ -34,12 +43,12 @@ def EC_3D(stretch, param):
     bstar = J**(-2.0/3.0) * dot(F, F.T) 
     lamChain = sqrt(trace(bstar)/3)
     devbstar = bstar - trace(bstar)/3 * eye(3) 
-    return param[0]/(J*lamChain) * invLangevin(lamChain/param[1]) / invLangevin(1/param[1]) * devbstar + param[2]*(J-1) * eye(3)
+    return param[0]/(J*lamChain) * inv_Langevin(lamChain/param[1]) / inv_Langevin(1/param[1]) * devbstar + param[2]*(J-1) * eye(3)
 
-def toVec(A):
+def to_vec(A): # Helper vector conversion function
     return array([A[0][0], A[1][1], A[2][2]], dtype='float64')
 
-def uniaxial_stress_visco(model, timeVec, trueStrainVec, params, aT):
+def uniaxial_stress_visco(model, timeVec, trueStrainVec, params, aT): # Simulates uniaxial stress conditions, completing procedure item 2
     stress = zeros(len(trueStrainVec))
     lam2_1 = 1.0
     FBv1 = array([1.0, 1.0, 1.0], dtype='float64')
@@ -59,7 +68,7 @@ def uniaxial_stress_visco(model, timeVec, trueStrainVec, params, aT):
         FBv1 = res[1]
     return stress
 
-def BB_timeDer_3D(Fv, t, params, time0, time1, F0, F1):
+def BB_timeDer_3D(Fv, t, params, time0, time1, F0, F1): # Simulates uniaxial stress conditions, completing procedure item 3
     mu, lamL, kappa, s, xi, C, tauBase, m, tauCut = params[:9]
     F = F0 + (t-time0) / (time1-time0) * (F1 - F0)
     Fe = F / Fv 
@@ -75,12 +84,12 @@ def BB_timeDer_3D(Fv, t, params, time0, time1, F0, F1):
     FvDot = prefac * (FeInv * devStress * F)
     return FvDot 
 
-def BB_3D(F0, F1, FBv0, time0, time1, params):
+def BB_3D(F0, F1, FBv0, time0, time1, params): # Elastic component function solver and final component
     muA, lamL, kappa, s = params[:4]
-    StressA = toVec(EC_3D(F1, [muA, lamL, kappa]))
+    StressA = to_vec(EC_3D(F1, [muA, lamL, kappa]))
     FBv1 = scipy.integrate.odeint(BB_timeDer_3D, FBv0, array([time0, time1], dtype='float64'), args=(params, time0, time1, F0, F1))[1]
     FBe1 = F1 / FBv1 
-    StressB = toVec(EC_3D(FBe1, [s*muA, lamL, kappa]))
+    StressB = to_vec(EC_3D(FBe1, [s*muA, lamL, kappa]))
     Stress = StressA + StressB 
     return (Stress, FBv1)
 
@@ -99,14 +108,16 @@ trueXylanStressSansTempChange = uniaxial_stress_visco(BB_3D, timeVec, trueStrain
 trueXylanStressWithTempChange = uniaxial_stress_visco(BB_3D, timeVec, trueStrain, xylanParams, aT)
 truePectinStressSansTempChange = uniaxial_stress_visco(BB_3D, timeVec, trueStrain, highMethoxylPectinParams, 1)
 truePectinStressWithTempChange = uniaxial_stress_visco(BB_3D, timeVec, trueStrain, highMethoxylPectinParams, aT)
-plot(trueStrain, trueCelluloseStressSansTempChange, 'b-', label='Cellulose without temp. conditions')
-plot(trueStrain, trueCelluloseStressWithTempChange, 'r-', label='Cellulose with temp. conditions')
-plot(trueStrain, trueXylanStressSansTempChange, 'g-', label='Xylan without temp. conditions')
-plot(trueStrain, trueXylanStressWithTempChange, 'm-', label='Xylan with temp. conditions')
-plot(trueStrain, truePectinStressSansTempChange, 'c-', label='HM Pectin without temp. conditions')
-plot(trueStrain, truePectinStressWithTempChange, 'y-', label='HM Pectin with temp. conditions')
-plt.legend(loc='upper left')
-xlabel('True Strain')
-ylabel('True Stress (MPa)')
-grid('on')
-show()
+
+if __name__ == "__main__":
+    plot(trueStrain, trueCelluloseStressSansTempChange, 'b-', label='Cellulose without temp. conditions')
+    plot(trueStrain, trueCelluloseStressWithTempChange, 'r-', label='Cellulose with temp. conditions')
+    plot(trueStrain, trueXylanStressSansTempChange, 'g-', label='Xylan without temp. conditions')
+    plot(trueStrain, trueXylanStressWithTempChange, 'm-', label='Xylan with temp. conditions')
+    plot(trueStrain, truePectinStressSansTempChange, 'c-', label='HM Pectin without temp. conditions')
+    plot(trueStrain, truePectinStressWithTempChange, 'y-', label='HM Pectin with temp. conditions')
+    plt.legend(loc='upper left')
+    xlabel('True Strain')
+    ylabel('True Stress (MPa)')
+    grid('on')
+    show()
